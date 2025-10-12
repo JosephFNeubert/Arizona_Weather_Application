1909cf068e5b18c14dbcc538f1b92f1c7fad5016
Joseph Neubert <joeyfneubert@gmail.com>
2025-10-11 22:53:37 -0700

    Imported image into assets for frontend and confirmed Streamlit functionality

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+# IMPORTS
 import streamlit as st
 import json
 import subprocess
 import pandas as pd
+from PIL import Image
 
 
+# Subprocess to run backend file to update JSON files
 def fetch_data():
     subprocess.run(["python", "ArizonaWeatherFetch.py"], check=True)
+
+
+# Frontend
+banner = Image.open("Assets/Banner.png")
+st.image(banner, use_container_width=True)
+st.text("Beginning of application website")
