11aac6f326b02999ba88b8549c3370ceac149013
Joseph Neubert <joeyfneubert@gmail.com>
2025-10-12 22:32:12 -0700

    First attempt at display data generated by subprocess on frontend (to be improved aesthetically soon)

--- app.py
+++ app.py
@@ -4,14 +4,36 @@ import json
 import subprocess
 import pandas as pd
 from PIL import Image
+from ArizonaWeatherFetch import cities_tuple
+
+data = {}
 
 
 # Subprocess to run backend file to update JSON files
-def fetch_data():
-    subprocess.run(["python", "ArizonaWeatherFetch.py"], check=True)
+def generate_data() -> None:
+    subprocess.run(["python", "ArizonaWeatherFetch.py"])
+
+
+# Graphically generate data
+def display_data(city: str) -> None:
+    if city == "Phoenix":
+        generate_data()
+        with open(cities_tuple[0], "r") as f:
+            data = json.load(f)
+        st.json(data)
+    elif city == "Prescott":
+        generate_data()
+        with open(cities_tuple[1], "r") as f:
+            data = json.load(f)
+        st.json(data)
+    else:
+        st.text("Unable to generate weather data. Try again later...")
 
 
 # Frontend
 banner = Image.open("Assets/Banner.png")
 st.image(banner, use_container_width=True)
-st.text("Beginning of application website")
+st.text("Please select a city to view its weather conditions.")
+city_select = st.selectbox("Select city", ["Phoenix", "Prescott"])
+if st.button("Load Data"):
+    display_data(city_select)
